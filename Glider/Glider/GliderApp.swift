dae5281b38f5d0806f49dd6417336f5b71cbe6ab
TopMyster <topmyster@outlook.com>
2026-05-18 14:13:16 -0500

    feat: Adds settings page and changes the background of the app to thin material

--- Glider/Glider/GliderApp.swift
+++ Glider/Glider/GliderApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct GliderApp: App {
+    @Environment(\.openWindow) private var openWindow
     var body: some Scene {
         WindowGroup {
-            ContentView().toolbarVisibility(.hidden)
-        }.windowStyle(.hiddenTitleBar)
+            ContentView()
+                .toolbar(removing: .title)
+                .containerBackground(.thinMaterial, for: .window)
+        }
+        
+        WindowGroup(id: "Settings") {
+            SettingsView()
+                .toolbar(removing: .title)
+                .containerBackground(.thinMaterial, for: .window)
+        }
+        
+        .commands {
+            CommandMenu("Window") {
+                Button("Open Settings") {
+                    openWindow(id: "Settings")
+                }.keyboardShortcut(.init("k"), modifiers: .command)
+            }
+        }
+    }
+    
+    #Preview {
+        ContentView()
     }
 }
